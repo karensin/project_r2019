c53af511618127e11449e732c93dec6343f9573a
Jason Jiang <jjiang1991@gmail.com>
2020-01-28 12:43:36 -0800

    converted everything to asyncawait

--- src/Test.js
+++ src/Test.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
-const domain = "https://api.petfinder.com"
+const domain = "https://api.petfinder.com";
+const tokenUrl = '/v2/oauth2/token';
+const url = `/v2/animals?type=dog&good_with_children=true`;;
 class Test extends React.Component {
     constructor(props) {
         super(props);
@@ -12,49 +14,64 @@ class Test extends React.Component {
         };
     }
 
-    componentDidMount() {
-        const url = `/v2/animals?type=dog&good_with_children=true`;
-        const tokenUrl = '/v2/oauth2/token';
-
-        fetch(`${domain}${tokenUrl}`, {
+    async getToken() {
+        const response = await fetch(`${domain}${tokenUrl}`, {
             body: "grant_type=client_credentials&client_id=cx1Q1hP2mvR6jeG447cARka8URjwpWlyn6myKedV3w6ap3qy0v&client_secret=j6t83Wt0nfH075v0hhdYnutWnnuvfmtnDbmY8JOb",
             headers: {
                 "Content-Type": "application/x-www-form-urlencoded"
             },
             method: 'POST'
-        })
-            .then(res => res.json())
-            .then(
-                (res) => {
-                    this.token = res.access_token;
-                    const bearer = `Bearer ${this.token}`;
-                    fetch(domain + url, {
-                        withCredentials: true,
-                        headers: {
-                            Authorization: bearer,
-                            'Content-Type': 'application/json'
-                        }
-                    })
-                        .then(res => res.json())
-                        .then(
-                            (result) => {
-                                this.setState({
-                                    isLoaded: true,
-                                    items: result.animals
-                                });
-                            },
-                            // Note: it's important to handle errors here
-                            // instead of a catch() block so that we don't swallow
-                            // exceptions from actual bugs in components.
-                            (error) => {
-                                this.setState({
-                                    isLoaded: true,
-                                    error
-                                });
-                            }
-                        )
+        });
+        const jsonResponse = await response.json();
+        this.token = jsonResponse.access_token;
+    }
+
+    async requestData() {
+        const bearer = `Bearer ${this.token}`;
+        try {
+            const res = await fetch(domain + url, {
+                withCredentials: true,
+                headers: {
+                    Authorization: bearer,
+                    'Content-Type': 'application/json'
                 }
-            )
+            });
+            const jsonRes = await res.json();
+            this.setState({
+                isLoaded: true,
+                items: jsonRes.animals
+            });
+        } catch (error) {
+            this.setState({
+                isLoaded: true,
+                error
+            });
+        }
+    }
+
+    async componentDidMount() {
+        await this.getToken();
+        console.log(this.token);
+        this.requestData();
+
+        // .then(res => res.json())
+        // .then(
+        //     (result) => {
+        //         this.setState({
+        //             isLoaded: true,
+        //             items: result.animals
+        //         });
+        //     },
+        //     // Note: it's important to handle errors here
+        //     // instead of a catch() block so that we don't swallow
+        //     // exceptions from actual bugs in components.
+        //     (error) => {
+        //         this.setState({
+        //             isLoaded: true,
+        //             error
+        //         });
+        //     }
+        // )
     }
 
     render() {
