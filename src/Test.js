808497d4758dcdd17ed2bcc791834f4661ad0786
Jason Jiang <jjiang1991@gmail.com>
2020-01-27 17:26:15 -0800

    get the request "working"

--- src/Test.js
+++ src/Test.js
@@ -11,14 +11,17 @@ class Test extends React.Component {
     }
 
     componentDidMount() {
-        var url = "https://api.petfinder.com/v2/animals?type=dog&good_with_children=true";
+        var url = "/v2/animals?type=dog&good_with_children=true";
         var bearer = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjQxMzRiNjE2NmE1MjlhY2ZmYjYxNGU2NGU0YTI4OWY5ZmE4NWFiYzZlZTgwZWZjOGQ2ZWEwMDk2MDFiOGY0N2YwODA4NGRlMGRhNTg3NmM1In0.eyJhdWQiOiJjeDFRMWhQMm12UjZqZUc0NDdjQVJrYThVUmp3cFdseW42bXlLZWRWM3c2YXAzcXkwdiIsImp0aSI6IjQxMzRiNjE2NmE1MjlhY2ZmYjYxNGU2NGU0YTI4OWY5ZmE4NWFiYzZlZTgwZWZjOGQ2ZWEwMDk2MDFiOGY0N2YwODA4NGRlMGRhNTg3NmM1IiwiaWF0IjoxNTgwMTY2NTQ5LCJuYmYiOjE1ODAxNjY1NDksImV4cCI6MTU4MDE3MDE0OSwic3ViIjoiIiwic2NvcGVzIjpbXX0.Z94K4eM3cPEIVNo_VQ5GyZjK0vPVYk0UXG5tqHUVeb7MBZ7HvNF756gjoGqbYscT5pIcB140UHY5lYbyELcDwkPpC2AZATfJt0dAUk2OREQuqQH99e7GxciAIgj0con_apaUml9674UEnjYfAJYepdokffNTa6R8kLTtv0af0s-T3crigMkzl16bfMqgedrr0HFMHHt31i_d_WLdHPc8yl9BRgEVjOAZULDazLDv0ALBSxymDYkKFf0iPjaqee5eHMoN-8QdpzaPvycMUxm3oszwkjPAwzTKnjemTaqcvERQ-FPMZM-2VwEwsuHpD06mmvR-QuQtY1yPuIZl95WWZQ'
 
         fetch(url, {
             withCredentials: true,
             headers: {
                 'Authorization': bearer,
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                "Access-Control-Allow-Origin": "*",
+                'Access-Control-Allow-Methods': "GET, PUT, POST, DELETE, HEAD, OPTIONS",
+                'Access-Control-Expose-Headers': 'Authorization'
             }
         }).then(res => res.json())
             .then(
@@ -26,8 +29,7 @@ class Test extends React.Component {
                     console.log(result)
                     this.setState({
                         isLoaded: true,
-                        items: result.items,
-                        "Access-Control-Allow-Origin": true
+                        items: result.items
                     });
                 },
                 // Note: it's important to handle errors here
@@ -43,22 +45,23 @@ class Test extends React.Component {
     }
 
     render() {
-        const { error, isLoaded, items } = this.state;
-        if (error) {
-            return <div>Error: {error.message}</div>;
-        } else if (!isLoaded) {
-            return <div>Loading...</div>;
-        } else {
-            return (
-                <ul>
-                    {items.map(item => (
-                        <li key={item.name}>
-                            {item.name} {item.price}
-                        </li>
-                    ))}
-                </ul>
-            );
-        }
+        return (<div></div>)
+        // const { error, isLoaded, items } = this.state;
+        // if (error) {
+        //     return <div>Error: {error.message}</div>;
+        // } else if (!isLoaded) {
+        //     return <div>Loading...</div>;
+        // } else {
+        //     return (
+        //         <ul>
+        //             {items.map(item => (
+        //                 <li key={item.name}>
+        //                     {item.name} {item.price}
+        //                 </li>
+        //             ))}
+        //         </ul>
+        //     );
+        // }
     }
 }
 export default Test; 
