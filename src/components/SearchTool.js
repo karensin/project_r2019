c696f9b5ec431bd3584f9c5918be23a77197c1e0
Karen k  sin <karenksin@MacBook-Pro.attlocal.net>
2020-01-29 09:12:18 -0800

    searchTool in progress

--- src/components/SearchTool.js
+++ src/components/SearchTool.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col, Button } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 import { Form, FormControl } from 'react-bootstrap'
-
+import { ButtonToolbar, Button, ToggleButton, ToggleButtonGroup } from 'react-bootstrap/'
 class SearchTool extends Component {
     constructor() {
         super()
 
-        // this.changeHandler = this.changeHandler.bind(this);
+        this.changeHandler = this.changeHandler.bind(this);
     }
 
 
 
 
-    changeHandler = (event) => {
-        console.log('hi')
+    changeHandler() {
+        console.log('it works')
     }
 
 
@@ -24,20 +24,19 @@ class SearchTool extends Component {
         return (
             <Container>
                 <Form>
-
-                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2 w-50" />
                     <Button variant="outline-light">Search</Button>
                     <Row>
                         <Col> <button>  Dog</button> </Col>
                         <Col> <button>  Cat</button> </Col>
                     </Row>
                     <Row>
-                        Age
+
                         <Col>  </Col>
                     </Row>
 
 
-                    {['checkbox'].map(type => (
+                    {/* {['checkbox'].map(type => (
                         <div key={`custom-inline-${type}`} className="mb-3">
                             <Form.Check
                                 custom
@@ -62,7 +61,36 @@ class SearchTool extends Component {
                                 id={`custom-inline-${type}-3`}
                             />
                         </div>
-                    ))}
+                    ))} */}
+                    Age
+                    <ButtonToolbar>
+                        <ToggleButtonGroup type="checkbox">
+                            <ToggleButton value={1}>Puppy</ToggleButton>
+                            <ToggleButton value={2}>Junior </ToggleButton>
+                            <ToggleButton value={3}>Adult</ToggleButton>      <ToggleButton value={4}>Senior</ToggleButton>
+
+                        </ToggleButtonGroup>
+                    </ButtonToolbar>
+                    Coat
+                    <ButtonToolbar>
+                        <ToggleButtonGroup type="checkbox" >
+                            <ToggleButton value={1}>Hairless</ToggleButton>
+                            <ToggleButton value={2}>Short </ToggleButton>
+                            <ToggleButton value={3}>Long</ToggleButton>      <ToggleButton value={4}>Wire</ToggleButton>
+                            <ToggleButton value={4}>Curly</ToggleButton>
+                        </ToggleButtonGroup>
+                    </ButtonToolbar>
+                    Good with
+                    <ButtonToolbar>
+                        <ToggleButtonGroup type="checkbox" >
+                            <ToggleButton value={1}>Children</ToggleButton>
+                            <ToggleButton value={2}>Dogs </ToggleButton>
+                            <ToggleButton value={3}>Cats</ToggleButton>
+                        </ToggleButtonGroup>
+                    </ButtonToolbar>
+
+
+
                     <Button type="submit">
                         Search
                             </Button>
