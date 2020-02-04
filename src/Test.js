234f49e39d4a7e3a8442e669b7a6f8be968f49ab
Karen k  sin <karenksin@MacBook-Pro.local>
2020-02-03 16:35:01 -0800

    fixing animaltype

--- src/Test.js
+++ src/Test.js
@@ -1,8 +1,7 @@
 import React from 'react'
 
-const domain = "https://api.petfinder.com";
-const tokenUrl = '/v2/oauth2/token';
-const url = `/v2/animals?type=dog&good_with_children=true`;;
+
+
 class Test extends React.Component {
     constructor(props) {
         super(props);
@@ -14,46 +13,8 @@ class Test extends React.Component {
         };
     }
 
-    async getToken() {
-        const response = await fetch(`${domain}${tokenUrl}`, {
-            body: "grant_type=client_credentials&client_id=cx1Q1hP2mvR6jeG447cARka8URjwpWlyn6myKedV3w6ap3qy0v&client_secret=j6t83Wt0nfH075v0hhdYnutWnnuvfmtnDbmY8JOb",
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded"
-            },
-            method: 'POST'
-        });
-        const jsonResponse = await response.json();
-        this.token = jsonResponse.access_token;
-    }
-
-    async requestData() {
-        const bearer = `Bearer ${this.token}`;
-        try {
-            const res = await fetch(domain + url, {
-                withCredentials: true,
-                headers: {
-                    Authorization: bearer,
-                    'Content-Type': 'application/json'
-                }
-            });
-            const jsonRes = await res.json();
-            this.setState({
-                isLoaded: true,
-                items: jsonRes.animals
-            });
-        } catch (error) {
-            this.setState({
-                isLoaded: true,
-                error
-            });
-        }
-    }
-
     async componentDidMount() {
-        await this.getToken();
         console.log(this.token);
-        this.requestData();
-
     }
 
     makeid(length) {
@@ -66,11 +27,12 @@ class Test extends React.Component {
         return result;
     }
     render() {
-        const { error, isLoaded, items } = this.state;
+        // this.requestData();
+        console.log('render', this.props)
+        let { items, isLoaded } = this.props;
+        items = items || [];
 
-        if (error) {
-            return <div>Error: {error.message}</div>;
-        } else if (!isLoaded) {
+        if (!isLoaded) {
             return <div>Loading...</div>;
         } else {
             return (
--- src/components/SearchTool.js
+++ src/components/SearchTool.js
@@ -4,75 +4,103 @@ import { Container, Row, Col } from 'react-bootstrap'
 import { Form, FormControl } from 'react-bootstrap'
 import { ButtonToolbar, Button, ToggleButton, ToggleButtonGroup } from 'react-bootstrap/'
 import Expand from './Expand.js';
-
-import Test from '../Test.js'
+import Test from '../Test.js';
+const domain = "https://api.petfinder.com";
+const tokenUrl = '/v2/oauth2/token';
+const url = `/v2/animals`;
 
 class SearchTool extends Component {
     constructor() {
         super()
+        this.state = {
+            petType: 'dog',
+        }
 
     }
 
 
+    async getToken() {
+        const response = await fetch(`${domain}${tokenUrl}`, {
+            body: "grant_type=client_credentials&client_id=cx1Q1hP2mvR6jeG447cARka8URjwpWlyn6myKedV3w6ap3qy0v&client_secret=j6t83Wt0nfH075v0hhdYnutWnnuvfmtnDbmY8JOb",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            method: 'POST'
+        });
+        const jsonResponse = await response.json();
+        this.token = jsonResponse.access_token;
+    }
 
+    async requestData() {
+        const params = {
+            type: this.state.petType,
+        }
+        console.log("trying to request Data", params);
+        var queryString = Object.keys(params).map(key => key + '=' + params[key]).join('&');
+
+        const bearer = `Bearer ${this.token}`;
+        console.log(domain + url + '?' + queryString);
+        try { //teach string interpolation 
+            const res = await fetch(domain + url + '?' + queryString, {
+                withCredentials: true,
+                headers: {
+                    Authorization: bearer,
+                    'Content-Type': 'application/json'
+                }
+            });
+            const jsonRes = await res.json();
+            this.setState({
+                isLoaded: true,
+                items: jsonRes.animals
+            });
+        } catch (error) {
+            console.log(error);
+            // this.setState({
+            //     isLoaded: true,
+            //     error
+            // });
+        }
+    }
 
     changeHandler = (event) => {
         console.log('it works')
     }
 
-
-
+    handleChange = val => {
+        console.log('val is', val);
+        this.setState({
+            petType: val
+        });
+        console.log('val is', val);
+        this.getToken();
+        this.requestData();
+    }
 
     render() {
+        console.log(this.state.items)
         return (
             <Container className="SearchToolBox">
                 <Form>
                     <FormControl type="text" placeholder="Search" className="mr-sm-2 w-50" />
                     <Button variant="outline-light">Search</Button>
                     <Row>
-                        <Col> <button>  Dog</button> </Col>
-                        <Col> <button>  Cat</button> </Col>
-                    </Row>
-                    <Row>
 
-                        <Col>  </Col>
-                    </Row>
+                        <ButtonToolbar>
+                            <ToggleButtonGroup type="radio" name="options" onChange={this.handleChange}>
+                                <ToggleButton value={'dog'}>Dog</ToggleButton>
+                                <ToggleButton value={'cat'}>Cat</ToggleButton>
+                            </ToggleButtonGroup>
+                        </ButtonToolbar>
 
-                    {/* 
-                    {['checkbox'].map(type => (
-                        <div key={`custom-inline-${type}`} className="mb-3">
-                            <Form.Check
-                                custom
-                                inline
-                                label="1"
-                                type={type}
-                                id={`custom-inline-${type}-1`}
-                                onChange={this.changeHandler}
-                            />
-                            <Form.Check
-                                custom
-                                inline
-                                label="2"
-                                type={type}
-                                id={`custom-inline-${type}-2`}
-                            />
-                            <Form.Check
-                                custom
-                                inline
-                                label="3"
-                                type={type}
-                                id={`custom-inline-${type}-3`}
-                            />
-                        </div>
-                    ))} */}
+                    </Row>
 
                     <Expand />
-                    <Button>
+                    <Button >
                         Search
-                            </Button>
+                    </Button>
 
                 </Form>
-                <Test />
+                <Test items={this.state.items} isLoaded={this.state.isLoaded} />
 
             </Container>
         )
