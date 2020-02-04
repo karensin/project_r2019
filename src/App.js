4747724999e2188352b1732bffcf125fa774d816
Jason Jiang <jjiang1991@gmail.com>
2020-02-03 16:46:47 -0800

    testing

--- src/App.js
+++ src/App.js
@@ -2,36 +2,17 @@ import './App.css';
 import React, { Component } from 'react';
 import Footer from './components/Footer.js'
 import Header from './components/Header.js'
-import Test from './Test.js'
-import LandingPage from './components/LandingPage'
 import SearchBar from './components/SearchBar.js'
 class App extends Component {
-
-<<<<<<< HEAD
-  render() {
-    return (
-      <div className="App">
-        <SearchBar />
-        <LandingPage />
-        <Header />
-
-        <Test />
-
-        {/* <About />
-        <Features />
-        <FAQ /> */}
-        <Footer />
-      </div>
-    )
-  }
-=======
-function App() {
-    return (
-        <div className="App">
-            <Test />
-        </div >
-    );
->>>>>>> clean up
+    render() {
+        return (
+            <div className="App">
+                <SearchBar />
+                <Header />
+                <Footer />
+            </div>
+        )
+    }
 }
 
 
