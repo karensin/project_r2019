2edca79081e4494b726a2c864c75bb80110d4cc5
Karen k  sin <karenksin@MacBook-Pro.attlocal.net>
2020-06-01 13:56:45 -0700

    Updates

---
+++ precache-manifest.167d3ce2571e5d1a70273fd8fb2d47fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d872bf8224be84bc36c8d0e5dd3cc041",
+    "url": "/project_r2019/index.html"
+  },
+  {
+    "revision": "dac2503561a671119e12",
+    "url": "/project_r2019/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "c89531bc4c94128e1e1d",
+    "url": "/project_r2019/static/css/main.2f28847c.chunk.css"
+  },
+  {
+    "revision": "dac2503561a671119e12",
+    "url": "/project_r2019/static/js/2.243225d5.chunk.js"
+  },
+  {
+    "revision": "c26adef31ea8c6ad4c1406ce2d90c098",
+    "url": "/project_r2019/static/js/2.243225d5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c89531bc4c94128e1e1d",
+    "url": "/project_r2019/static/js/main.e69a588a.chunk.js"
+  },
+  {
+    "revision": "8a977baa6f68c938620d",
+    "url": "/project_r2019/static/js/runtime-main.99cc7944.js"
+  }
+]);
